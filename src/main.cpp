0087aa7f9120853bc79578275985883cf419a6dc
Ward De Ridder <ward.deridder@ap.be>
2022-08-01 22:59:57 +0200

    add LCD support

--- src/main.cpp
+++ src/main.cpp
@@ -37,11 +37,44 @@ void blink() {
 }
 
 
+//tft
+Badge2020_TFT tft;
+//put values on screen
+void updateTFT() {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setTextSize(2);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setCursor(10, 10);
+  tft.print("millis: " + String(millis()));
+  tft.setCursor(10, 30);
+  if (touchRead(27) > 30){
+    tft.setTextColor(ST77XX_RED);
+  } else {
+    tft.setTextColor(ST77XX_GREEN);
+  }
+  tft.print("Touch 0: " + String(touchRead(27)));
+  tft.setCursor(10, 50);
+  if (touchRead(14) > 30){
+    tft.setTextColor(ST77XX_RED);
+  } else {
+    tft.setTextColor(ST77XX_GREEN);
+  }
+  tft.print("Touch 1: " + String(touchRead(14)));
+  tft.setCursor(10, 70);
+  if (touchRead(13) > 30){
+    tft.setTextColor(ST77XX_RED);
+  } else {
+    tft.setTextColor(ST77XX_GREEN);
+  }
+  tft.print("Touch 2: " + String(touchRead(13)));
+}
 
 
 void setup() {
   pinMode(ledPin, OUTPUT);
   buzzer_once();
+  tft.init(240, 240);
+  tft.setRotation( 2 );
 }
 
 
@@ -49,4 +82,6 @@ void setup() {
 void loop() {
   //heartbeat
   blink();
+  updateTFT();
+  delay(20);
 }
